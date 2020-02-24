c0990d00a216b9081338427431fd4e6ab817efec
Niranjan <niranjan.balachandrareddy@alliancerxwp.com>
2020-02-24 15:11:25 +0530

    Lazy Loading

---
+++ src/app/Auth/auth-routing.module.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [{
+    path: '', component: AuthComponent,
+}];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule {
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { AuthguardService } from './shared/authguard.service';
+import { RouterModule, Routes } from '@angular/router';
 import { CanDeactivateGuard } from './shopping-list/shopping-edit/can-deactivate-guard';
-import { RecipeResolverService } from './recipes/recipes-resolver.service';
-import { AuthComponent } from './Auth/auth.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipe', pathMatch: 'full' },
+  { path: '', redirectTo: 'recipe', pathMatch: 'full' },
   {
-    path: 'recipe', canActivateChild: [AuthguardService], component: RecipesComponent, children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipesDetailComponent, resolve: [RecipeResolverService] },
-      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] }
-    ]
+    path: 'recipe',
+    loadChildren: '../app/recipes/recipes.module#RecipesModule'
   },
-  { path: 'shop',canDeactivate:[CanDeactivateGuard], component: ShoppingListComponent },
-  { path: 'auth', component: AuthComponent },
+  {
+    path: 'shop',
+    canDeactivate: [CanDeactivateGuard],
+    component: ShoppingListComponent
+  },
+  {
+    path: 'auth',
+    loadChildren: '../app/Auth/auth.module#AuthModule'
+  },
   { path: '**', redirectTo: '/' }
 ];
 
